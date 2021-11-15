77e5d664fca47bc589085165a9a0056733ff5615
Jade <jadely0819@gmail.com>
2021-11-15 22:38:23 +0900

    Edit file

--- Programmers/SQL/SELECT.sql
+++ Programmers/SQL/SELECT.sql
@@ -15,3 +15,19 @@ SEX_UPON_INTAKE	VARCHAR(N)	FALSE
 SELECT *
 FROM ANIMAL_INS
 GROUP BY ANIMAL_ID
+
+# P2 - 역순 정렬하기
+# 동물 보호소에 들어온 모든 동물의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요. 
+# 이때 결과는 ANIMAL_ID 역순으로 보여주세요. SQL을 실행하면 다음과 같이 출력되어야 합니다.
+
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC
+
+# P3 - 아픈 동물 찾기
+# 동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 
+# 이때 결과는 아이디 순으로 조회해주세요.
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
